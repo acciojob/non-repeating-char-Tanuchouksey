82903ba43a9c6c3c3cc959ca3157d84c606f402c
Tanuchouksey <109014772+Tanuchouksey@users.noreply.github.com>
2023-09-28 23:28:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-	let n =str.length(); 
-	let ans=""; 
-	for (let i=0; i<n;i++) {
-		var c=str.charAt(i);
-		if(ans.isEmpty())
-		{ 
-			ans=ans+c;
-		}
-		else if(c--str.charAt(i+1)
-		{
-			ans.splice(0,-1)
-		}
-		else 
-		{
-			return ans;
-		}
-	}
-	
+  if (str.length === 0) {
+    return null;
+  }
+
+  // Create a frequency map to count the occurrences of each character
+  const charCount = new Map();
+
+  // Populate the frequency map
+  for (const char of str) {
+    if (charCount.has(char)) {
+      charCount.set(char, charCount.get(char) + 1);
+    } else {
+      charCount.set(char, 1);
+    }
+  }
+
+  // Iterate through the string again to find the first non-repeated character
+  for (const char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
+
+  // If no non-repeated character is found, return null
+  return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+// Test cases
+console.log(firstNonRepeatedChar('aabbcdd')); // Output: 'c'
+console.log(firstNonRepeatedChar('aabbcc'));  // Output: null
+This function first creates a frequency map to count the occurrences of each character in the string, and then it iterates through the string to find the first character with a frequency of 1. If no such character is found, it returns null.
+
+
+
+
+
