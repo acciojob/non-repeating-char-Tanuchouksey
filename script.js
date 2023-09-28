9ca719fe4648faac6bbfde208f07a03a795061ef
Tanuchouksey <109014772+Tanuchouksey@users.noreply.github.com>
2023-09-28 21:43:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n =str.length(); 
+	let ans=""; 
+	for (let i=0; i<n;i++) {
+		var c=str.charAt(i);
+		if(ans.isEmpty())
+		{ 
+			ans=ans+c;
+		}
+		else if(c--str.charAt(i+1)
+		{
+			ans.splice(0,-1)
+		}
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
